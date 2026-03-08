a1fada7b3b2dfc5f9277fd53fb179af142b514c9
beny81 <pbenythree3@gmail.com>
2026-03-07 21:59:56 -0500

    send dist

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
 <template>
   <main>
     <div>
-      hello G, welcome to OurWeddingCloud, the best place to share your wedding moments with your loved ones. We are currently working hard to bring you an amazing experience, so stay tuned and check back soon for updates! In the meantime, feel free to explore our website and discover all the features we have in store for you. Thank you for being part of our community!
+      hello G, welcome to OurWeddingCloud, the best place to share your wedding moments with your
+      loved ones. We are currently working hard to bring you an amazing experience, so stay tuned
+      and check back soon for updates! In the meantime, feel free to explore our website and
+      discover all the features we have in store for you. Thank you for being part of our community!
     </div>
     <!-- <RouterView /> -->
   </main>
